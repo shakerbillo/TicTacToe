ebbfe0d57ac589f003539be6e794277a2e21e987
Emmanuel Gyan-Amoah <82492796+shakerbillo@users.noreply.github.com>
2021-12-22 10:32:23 +0100

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -13,14 +13,7 @@ public class TicTacToe {
             };
 
        
-              
-           
-
-             
-          
-            //Task 1: Create an array with three rows of '_' characters.
-
-            //Task 2: Call the function printBoard();
+    
             printBoard(board);
            
             
@@ -58,30 +51,6 @@ public class TicTacToe {
               
 
 
-
-              /*
-              {  Task 3: Loop through turns.
-          
-
-                  if (X) turn {
-                     Task 4: call askUser(). 
-                     Task 5: populate the board using askUser's return value.
-                  } else {
-                      Task 4: call askUser(). 
-                      Task 5: populate the board using askUser's return value. Then, print it.
-
-                  }
-                  
-                Task 6 - Call the function.
-                   if return value == 3 {
-                     print: X wins and break the loop
-                  } else if return value == -3 {
-                     print: O wins and break the loop
-                  }
-
-              } 
-              */
-
             scan.close();
         }
       
@@ -100,26 +69,12 @@ public class TicTacToe {
         }
 
 
-    /** Task 2 - Write a function that prints the board.
-     * Function name - printBoard()
-     * @param board (char[][])
-     * 
-     * Inside the function:
-     *   1. print a new line.
-     *   2. print the board.
-     *      • separate each row by two lines. 
-     *      • each row precedes a tab of space
-     *      • each character in the grid has one space from the other character
-     */        
-
-
 
     public static int [] askUser(char[][] board){
       System.out.print(" • pick a row and column: ");
       int row = scan.nextInt();
       int element = scan.nextInt();
       while(board[row][element] != '_'){
-      // while(board[row][element] == 'X' || board[row][element] == 'O'){ longer way to write 
         System.out.print("Spot taken, try again: ");
         row =scan.nextInt();
         element = scan.nextInt();
@@ -136,20 +91,7 @@ public class TicTacToe {
           
 
 
-       
 
-   
-   /** Task 4 - Write a function that lets the user choose a spot
-     * Function name – askUser
-     * @param board (char[][] board)
-     * @return spot (int[])
-     * 
-     * Inside the function
-     *   1. Asks the user: • pick a row and column number: 
-     *   2. Check if the spot is taken. If so, let the user choose again.
-     *   3. Return the row and column in an int[] array.
-     * 
-     */
 
      public static int checkWin(char[][] board){
        int count = 0;
@@ -226,18 +168,7 @@ public class TicTacToe {
 
      }
 
-    /** Task 6 - Write a function that determines the winner
-     * Function name - checkWin 
-     * @param board (char[][])
-     * @return count (int)
-     * 
-     * Inside the function:
-     *   1. Make a count variable that starts at 0.
-     *   2. Check every row for a straight X or straight O (Task 7).
-     *   3. Check every column for a straight X or straight O (Task 8).
-     *   4. Check the left diagonal for a straight X or straight O (Task 9).
-     *   5. Check the right diagonal for a straight X or straight O (Task 10).
-     */
+  
 
 
   }
